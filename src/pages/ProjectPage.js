961852aba4b208ad5e7602989ffd42986ee5c4f5
diwasneupane <diwas.neupane55@gmail.com>
2024-04-23 03:27:10 +0545

    ui design project completed

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,213 +1,256 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Container, Table, Modal, Button, Form } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlus,
-  faPenToSquare,
+  faEdit,
   faTrashAlt,
+  faBorderNone,
 } from "@fortawesome/free-solid-svg-icons";
-import Modal from "react-bootstrap/Modal"; // Using react-bootstrap for modal
-import { Button, Table, Form } from "react-bootstrap"; // Using react-bootstrap for simplicity
-import { getAuthToken } from "../utils/Auth"; // Utility function to get the token
+import { getAuthToken } from "../utils/Auth";
+import axios from "axios";
 
 const ProjectPage = () => {
   const [projects, setProjects] = useState([]);
-  const [modalOpen, setModalOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   const [editData, setEditData] = useState(null);
-  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    fetchProjects(); // Fetch projects when component mounts
+    fetchProjects();
   }, []);
 
   const fetchProjects = async () => {
     try {
-      const token = getAuthToken(); // Retrieve the token
       const response = await axios.get("/api/v1/project/Projects", {
-        headers: {
-          Authorization: `Bearer ${token}`, // Pass the token in the header
-        },
+        headers: { Authorization: `Bearer ${getAuthToken()}` },
       });
-      setProjects(response.data); // Store the data in the state
+      setProjects(response.data);
     } catch (error) {
       console.error("Error fetching projects:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
-  const handleAddOrUpdateProject = async () => {
-    const token = getAuthToken(); // Get the token before making a request
-    const projectData = {
-      title: document.getElementById("projectTitle").value,
-      description: document.getElementById("projectDescription").value,
-      startDate: document.getElementById("projectStartDate").value,
-      endDate: document.getElementById("projectEndDate").value,
-      status: document.getElementById("projectStatus").value,
-    };
-
-    try {
-      if (editMode && editData) {
-        await axios.patch(
-          `/api/v1/project/updateProjects/${editData._id}`,
-          projectData,
-          {
-            headers: { Authorization: `Bearer ${token}` }, // Pass the token in the header
-          }
-        );
-      } else {
-        await axios.post("/api/v1/project/addProjects", projectData, {
-          headers: { Authorization: `Bearer ${token}` }, // Pass the token in the header
-        });
-      }
-
-      fetchProjects(); // Refresh the project list after add/update
-      setModalOpen(false); // Close the modal after success
-    } catch (error) {
-      console.error("Error saving project:", error);
-    }
-  };
-
-  const handleDeleteProject = async (projectId) => {
-    const token = getAuthToken(); // Get the token before making a request
+  const handleDelete = async (projectId) => {
     try {
       await axios.delete(`/api/v1/project/deleteProjects/${projectId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`, // Pass the token in the header
-        },
+        headers: { Authorization: `Bearer ${getAuthToken()}` },
       });
-      fetchProjects(); // Refresh the list after deletion
+      setProjects(projects.filter((p) => p._id !== projectId));
     } catch (error) {
       console.error("Error deleting project:", error);
     }
   };
 
-  const openAddModal = () => {
-    setEditMode(false);
-    setEditData(null);
-    setModalOpen(true);
+  const toggleModal = () => {
+    setEditData(null); // Reset editData when toggling the modal
+    setShowModal(!showModal);
   };
 
-  const openEditModal = (project) => {
-    setEditMode(true);
-    setEditData(project);
-    setModalOpen(true);
+  const handleEdit = (project) => {
+    setEditData(project); // Set editData when editing a project
+    setShowModal(true);
+  };
+
+  const handleSubmit = async (project) => {
+    const endpoint = editData
+      ? `/api/v1/project/updateProjects/${editData._id}`
+      : "/api/v1/project/addProjects";
+
+    const method = editData ? "PATCH" : "POST";
+
+    try {
+      await axios({
+        method,
+        url: endpoint,
+        data: project,
+        headers: { Authorization: `Bearer ${getAuthToken()}` },
+      });
+
+      fetchProjects(); // Refresh the project list after saving
+      toggleModal(); // Close the modal
+    } catch (error) {
+      console.error("Error saving project:", error);
+    }
   };
 
   return (
-    <div className="container">
-      <div className="row mb-3">
-        <div className="col">
-          <Button variant="primary" onClick={openAddModal}>
-            <FontAwesomeIcon icon={faPlus} /> Add Project
-          </Button>
-        </div>
-      </div>
+    <Container
+      className=" p-4"
+      style={{ backgroundColor: "white", width: "97%", borderRadius: "5px" }}
+    >
+      <Button
+        style={{
+          borderRadius: "25px",
+          border: "0",
+          backgroundColor: "#2DBFCD",
+        }}
+        variant="primary"
+        onClick={toggleModal}
+      >
+        <FontAwesomeIcon icon={faPlus} style={{ color: "white" }} /> Add Project
+      </Button>
 
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Project Title</th>
-            <th>Description</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-            <th>Status</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {projects.length > 0 ? (
-            projects.map((project) => (
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <Table className="table customTable mt-3">
+          <thead>
+            <tr>
+              <th>Project Title</th>
+              <th>Description</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th>Status</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody className="tableData" width={"40%"}>
+            {projects.map((project) => (
               <tr key={project._id}>
-                <td>{project.title}</td>
+                <td className="tableData">{project.title}</td>
                 <td>{project.description}</td>
                 <td>{new Date(project.startDate).toLocaleDateString()}</td>
                 <td>{new Date(project.endDate).toLocaleDateString()}</td>
                 <td>{project.status}</td>
                 <td>
-                  <FontAwesomeIcon
-                    icon={faPenToSquare}
-                    className="text-primary"
-                    onClick={() => openEditModal(project)}
-                  />
-                  {" | "}
-                  <FontAwesomeIcon
-                    icon={faTrashAlt}
-                    className="text-danger"
-                    onClick={() => handleDeleteProject(project._id)}
-                  />
+                  <Button variant="link" onClick={() => handleEdit(project)}>
+                    <FontAwesomeIcon
+                      icon={faEdit}
+                      style={{ color: "orange" }}
+                    />
+                  </Button>
+                  <Button
+                    variant="link"
+                    onClick={() => handleDelete(project._id)}
+                  >
+                    <FontAwesomeIcon
+                      icon={faTrashAlt}
+                      style={{ color: "red" }}
+                    />
+                  </Button>
                 </td>
               </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={6}>No projects found</td>
-            </tr>
-          )}
-        </tbody>
-      </Table>
+            ))}
+          </tbody>
+        </Table>
+      )}
 
-      {/* Modal for adding or editing projects */}
-      <Modal show={modalOpen} onHide={() => setModalOpen(false)}>
+      <Modal show={showModal} onHide={toggleModal}>
         <Modal.Header closeButton>
-          <Modal.Title>{editMode ? "Edit Project" : "Add Project"}</Modal.Title>
+          <Modal.Title>{editData ? "Edit Project" : "Add Project"}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group>
-              <Form.Label>Project Title</Form.Label>
-              <Form.Control
-                type="text"
-                id="projectTitle"
-                defaultValue={editMode && editData ? editData.title : ""}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Description</Form.Label>
-              <Form.Control
-                as="textarea"
-                id="projectDescription"
-                defaultValue={editMode && editData ? editData.description : ""}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Start Date</Form.Label>
-              <Form.Control
-                type="date"
-                id="projectStartDate"
-                defaultValue={
-                  editMode && editData ? editData.startDate.split("T")[0] : ""
-                }
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>End Date</Form.Label>
-              <Form.Control
-                type="date"
-                id="projectEndDate"
-                defaultValue={
-                  editMode && editData ? editData.endDate.split("T")[0] : ""
-                }
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Status</Form.Label>
-              <Form.Control as="select" id="projectStatus">
-                <option value="Pending">Pending</option>
-                <option value="Ongoing">Ongoing</option>
-                <option value="Completed">Completed</option>
-              </Form.Control>
-            </Form.Group>
-          </Form>
+          <ProjectForm
+            initialData={editData}
+            onSubmit={handleSubmit}
+            onClose={toggleModal}
+          />
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setModalOpen(false)}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={handleAddOrUpdateProject}>
-            {editMode ? "Update" : "Add"}
-          </Button>
-        </Modal.Footer>
       </Modal>
-    </div>
+    </Container>
+  );
+};
+
+const ProjectForm = ({ initialData, onSubmit, onClose }) => {
+  const [formData, setFormData] = useState({
+    title: initialData?.title || "",
+    description: initialData?.description || "",
+    startDate: initialData?.startDate || "",
+    endDate: initialData?.endDate || "",
+    status: initialData?.status || "Pending",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFormSubmit = () => {
+    if (onSubmit) {
+      onSubmit(formData);
+    }
+  };
+
+  return (
+    <Form className="container-fluid">
+      <Form.Group>
+        <Form.Label>Project Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label className="col-md-9">Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label className="col-md-3 mt-1">Start Date</Form.Label>
+        <Form.Control
+          type="date"
+          name="startDate"
+          value={formData.startDate}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label className="col-md-3 mt-1">End Date</Form.Label>
+        <Form.Control
+          type="date"
+          name="endDate"
+          value={formData.endDate}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label className="col-md-3 mt-1">Status</Form.Label>
+        <Form.Control
+          as="select"
+          name="status"
+          value={formData.status}
+          onChange={handleChange}
+        >
+          <option value="Pending">Pending</option>
+          <option value="Ongoing">Ongoing</option>
+          <option value="Completed">Completed</option>
+        </Form.Control>
+      </Form.Group>
+      <br />
+      <Button
+        style={{
+          borderRadius: "5px",
+          border: "0",
+          backgroundColor: "#2DBFCD",
+          marginRight: "5px",
+        }}
+        variant="primary"
+        onClick={handleFormSubmit}
+      >
+        Submit
+      </Button>
+
+      <Button
+        style={{
+          borderRadius: "5px",
+          border: "0",
+          backgroundColor: "#FFA500",
+        }}
+        variant="secondary"
+        onClick={onClose}
+      >
+        Close
+      </Button>
+    </Form>
   );
 };
 
